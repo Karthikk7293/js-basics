ca68948d305d9026b4e8516e63d3c820fca95127
Karthikk7293 <karthikk7293@gmail.com>
2025-05-27 08:56:58 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,85 +1,45 @@
-const submitButton = document.getElementById('submit-button')
+const clickMe = document.getElementById('click-me')
 
-submitButton.addEventListener('click', (e) => {
+console.log(clickMe);
+const container = document.getElementById('container')
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/
+
+console.log(container);
+clickMe.addEventListener('click', (e) => {
     e.preventDefault()
-    const name = document.getElementById('name-input')
-    const email = document.getElementById('email-input')
-    const phoneNumber = document.getElementById('phone-input')
-    const password = document.getElementById('password-input')
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const phoneRegex = /^\d{3}-?\d{3}-?\d{4}$/;
-    const passwordRegex = /^\d{8,12}$/;
-    let errorFlag = false;
+    let para = document.getElementById('error-value')
 
-    if (!name.value) {
-        name.style.borderColor = 'red'
-        name.style.borderWidth = '2px'
-        alert('please enter you name')
-        errorFlag = true
+    if (!para) {
+        para = document.createElement('p')
+        para.setAttribute('id', 'error-value')
+    }
 
+    const input = document.getElementById('input')
+    const errorContainer = document.getElementById('error-content')
+
+    input.style.borderColor = 'red'
+    para.style.color = 'red'
+
+    if (!input.value) {
+        para.innerHTML = 'Please enter your email';
+    } else if (!emailRegex.test(input.value)) {
+        para.innerHTML = 'Please enter a valid email';
     } else {
-        name.style.borderColor = 'transparent'
+        input.style.borderColor = 'green'
+        para.style.color = 'green'
+        para.innerHTML = 'email validation successfull!';
     }
+    console.log(errorContainer);
 
-    if (!email.value) {
-        email.style.borderColor = 'red'
-        email.style.borderWidth = '2px'
-        alert('please enter you email')
-        errorFlag = true
+    console.log(para);
 
-    } else {
-        if (emailRegex.test(email.value)) {
-            email.style.borderColor = 'transparent'
-        } else {
-            email.style.borderColor = 'red'
-            email.style.borderWidth = '2px'
-            alert('please enter a valid email')
-            errorFlag = true
+    errorContainer.appendChild(para)
 
-        }
-    }
-
-    if (!phoneNumber.value) {
-        phoneNumber.style.borderColor = 'red'
-        phoneNumber.style.borderWidth = '2px'
-        alert('please enter your Phone number')
-        errorFlag = true
-    } else {
-        if (phoneRegex.test(phoneNumber.value)) {
-            phoneNumber.style.borderColor = 'transparent'
-        } else {
-            phoneNumber.style.borderColor = 'red'
-            phoneNumber.style.borderWidth = '2px'
-            alert('please enter a valid phone number')
-            errorFlag = true
-        }
-    }
-
-    if (!password.value) {
-        password.style.borderColor = 'red'
-        password.style.borderWidth = '2px'
-        alert('please enter your password ')
-        errorFlag = true
-    } else {
-        if (passwordRegex.test(password.value)) {
-            password.style.borderColor = 'transparent'
-        } else {
-            password.style.borderColor = 'red'
-            password.style.borderWidth = '2px'
-            alert('password must contain morethan 8 digits')
-            errorFlag = true
-        }
-    }
-
-    if (!errorFlag) {
-        alert("Registration completed!")
-    }
-
-
-
-
-
+    setTimeout(() => {
+        para.remove()
+    }, 1000);
 
 
 

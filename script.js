beaf8aaa9c90594d7800bdfaf00d41e7f347e251
Karthikk7293 <karthikk7293@gmail.com>
2025-05-28 21:58:18 +0530

    APIs - fetch API,then, catch, JSON and Objects

--- script.js
+++ script.js
@@ -1,86 +1,73 @@
-const submitButton = document.getElementById('submit-button')
+// json => javascripit object notation 
 
-submitButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    const name = document.getElementById('name-input')
-    const email = document.getElementById('email-input')
-    const phoneNumber = document.getElementById('phone-input')
-    const password = document.getElementById('password-input')
+const data = {
+    "key": "value",
+    "age": "10",
+    "flag": "true"
+}
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const phoneRegex = /^\d{3}-?\d{3}-?\d{4}$/;
-    const passwordRegex = /^\d{8,12}$/;
-    let errorFlag = false;
+const obj = {
+    key: "value",
+    age: 10,
+    flag: true
+}
 
-    if (!name.value) {
-        name.style.borderColor = 'red'
-        name.style.borderWidth = '2px'
-        alert('please enter you name')
-        errorFlag = true
+document.getElementById('click-me').addEventListener('click', () => {
 
-    } else {
-        name.style.borderColor = 'transparent'
-    }
+    fetch('https://fakestoreapi.com/products').then((response) => {
+        console.log({ response });
+        return response.json()
+    }).then((result) => {
+        console.log({ result });
+        result.id = 10
+        return result
+    }).then((res) => {
+        console.log({ res });
 
-    if (!email.value) {
-        email.style.borderColor = 'red'
-        email.style.borderWidth = '2px'
-        alert('please enter you email')
-        errorFlag = true
+        for (let i = 0; i < res.length; i++) {
 
-    } else {
-        if (emailRegex.test(email.value)) {
-            email.style.borderColor = 'transparent'
-        } else {
-            email.style.borderColor = 'red'
-            email.style.borderWidth = '2px'
-            alert('please enter a valid email')
-            errorFlag = true
+            const row = document.getElementById('row')
+            const col = document.createElement('div')
+            const card = document.createElement('div')
+            const title = document.createElement('h6')
+            const image = document.createElement('img')
+            const description = document.createElement('p')
+
+            col.setAttribute('class', 'col-4 mx-auto  py-4')
+            card.setAttribute('class', 'card shadow border border-success rounded  p-3')
+
+            title.innerHTML = res[i].title
+            title.setAttribute('class', 'text-success font-bold')
+
+            image.src = res[i].image
+            image.style.width = '8rem'
+            image.setAttribute('class', 'mx-auto')
+
+            description.innerHTML = res[i].description
+            description.setAttribute('class', 'text-info font-bold')
+            description.style.fontSize = '12px'
+
+            card.appendChild(image)
+            card.appendChild(title)
+            card.appendChild(description)
+            col.appendChild(card)
+            console.log(row);
+
+            row.appendChild(col)
+        }
+
+
+
+
+
+    }).catch((err) => {
+        const error = err.toString()
+
+        if (error.startsWith("S")) {
+            alert("something went wrong")
 
         }
-    }
-
-    if (!phoneNumber.value) {
-        phoneNumber.style.borderColor = 'red'
-        phoneNumber.style.borderWidth = '2px'
-        alert('please enter your Phone number')
-        errorFlag = true
-    } else {
-        if (phoneRegex.test(phoneNumber.value)) {
-            phoneNumber.style.borderColor = 'transparent'
-        } else {
-            phoneNumber.style.borderColor = 'red'
-            phoneNumber.style.borderWidth = '2px'
-            alert('please enter a valid phone number')
-            errorFlag = true
-        }
-    }
-
-    if (!password.value) {
-        password.style.borderColor = 'red'
-        password.style.borderWidth = '2px'
-        alert('please enter your password ')
-        errorFlag = true
-    } else {
-        if (passwordRegex.test(password.value)) {
-            password.style.borderColor = 'transparent'
-        } else {
-            password.style.borderColor = 'red'
-            password.style.borderWidth = '2px'
-            alert('password must contain morethan 8 digits')
-            errorFlag = true
-        }
-    }
-
-    if (!errorFlag) {
-        alert("Registration completed!")
-    }
 
 
-
-
-
-
-
-
+    })
 })
